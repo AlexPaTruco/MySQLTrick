9e987f6bc1409d035238abd004dcdfbcd759a52a
Alexander <alexander.db27@gmail.com>
2014-05-31 15:29:43 -0400

    Updated index.php

--- index.php
+++ index.php
@@ -3,11 +3,3 @@
 require_once 'mysqltrick.php';
 
 $connection = new mysqlTrickClient();
-
-$trickhub = $connection->selectDB("tokenspot");
-
-//$notes = $trickhub->selectTable("notes");
-
-
-print_r($trickhub->getCurrentDB());
-
